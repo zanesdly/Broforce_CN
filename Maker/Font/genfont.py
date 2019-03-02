227371fb305c77aaf99744edd73b70299d358778
VirtualCup <wuliaoa1234567@163.com>
2019-03-03 05:59:02 +0800

    删除不必要的文件

--- Maker/Font/genfont.py
+++
@@ -1,6 +0,0 @@
-from UnityFont import UnityFont
-
-font = UnityFont("24.dat");
-font.character_spacing = 2;
-font.test();
-font.convert_to_raw("24.nw.dat", "C:/Windows/Fonts/SourceHanSansSC-Bold.otf");
